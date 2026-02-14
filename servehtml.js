fb19b0772428dee8a35723ef6c84c23dc9a5f49e
muhammedshamilmt <muhammedhishamptb@gmail.com>
2026-02-14 16:30:42 +0530

    html served

---
+++ servehtml.js
@@ -0,0 +1,14 @@
+
+
+var http=require('http');
+var fs=require('fs');
+http.createServer(function(req,res){
+    fs.readFile('sample.html',function(err,data){
+    res.writeHead(200,{'Content-type':'text/html'})
+    res.write(data);
+    res.end();
+    })
+}).listen(8888)
+
+ 
+    
--- servercreation.js
+++ servercreation.js
@@ -2,7 +2,7 @@ var http=require('http');
 http.createServer(server).listen(8888);
 
 function server(req,res){
-    res.write("Server is running");
+    res.write("Server test");
     res.end();
 }
 // a simple server creation using node js
