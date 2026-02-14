a302930f152da02a883410fc15ca17569a261e5c
muhammedshamilmt <muhammedhishamptb@gmail.com>
2026-02-14 12:28:43 +0530

    server creation completed

--- servercreation.js
+++ servercreation.js
@@ -5,3 +5,11 @@ function server(req,res){
     res.write("Server is running");
     res.end();
 }
+// a simple server creation using node js
+// ithile function server() is called when the server receives a request and it sends a response back to the client. The server listens on port 8888 for incoming requests.
+// another way
+var http=require('http');
+var server=http.createServer(function(req,res){
+    res.write("Server is running");
+    res.end();
+});
